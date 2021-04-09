b47b535c8c586483e1a0f2803b185f35c1048a67
ranoke <ranokpvp@gmail.com>
2021-04-09 03:00:01 +0300

    fix items

---
+++ res/shaders/materials.fs
@@ -0,0 +1,48 @@
+#version 330
+
+out vec4 frag_color;
+
+
+struct material_t {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;    
+    float shininess;
+}; 
+
+struct light_t {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+  
+uniform vec3 u_view_pos;
+uniform material_t u_material;
+uniform light_t u_light;
+
+in vec3 frag_pos;
+in vec3 normal;
+
+
+void main()
+{
+    // ambient
+    vec3 ambient = u_light.ambient * u_material.ambient;
+  	
+    // diffuse 
+    vec3 norm = normalize(normal);
+    vec3 light_dir = normalize(u_light.position - frag_pos);
+    float diff = max(dot(norm, light_dir), 0.0);
+    vec3 diffuse = u_light.diffuse * (diff * u_material.diffuse);
+    
+    // specular
+    vec3 view_dir = normalize(u_view_pos - frag_pos);
+    vec3 reflect_dir = reflect(-light_dir, norm);  
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), u_material.shininess);
+    vec3 specular = u_light.specular * (spec * u_material.specular);  
+        
+    vec3 result = ambient + diffuse + specular;
+    frag_color = vec4(result, 1.0);
+} 
---
+++ res/shaders/materials.vs
@@ -0,0 +1,18 @@
+#version 330
+layout (location = 0) in vec3 a_pos;
+layout (location = 1) in vec2 a_uv;
+layout (location = 2) in vec3 a_normal;
+
+out vec3 frag_pos;
+out vec3 normal;
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_proj;
+
+void main()
+{
+    normal = mat3(transpose(inverse(u_model))) * a_pos;  
+    
+    gl_Position = u_proj * u_view * u_model * vec4(a_pos, 1.0);
+}
